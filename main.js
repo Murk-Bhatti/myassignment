49ced312a8016f77c85d0fecfad19291a49e0d81
Murk-Bhatti <165047752+Murk-Bhatti@users.noreply.github.com>
2024-05-02 19:03:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//QUESTION 1:
+console.log('Hello World');
+//QUESTION 2:Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var personName = ('Habeeba');
+var message = ("how would you like to attend today's IT session, Online or Onsite?");
+console.log(personName + " " + message);

10840af5c8b8c2d24775a0737e45362d1577abad
Murk-Bhatti <165047752+Murk-Bhatti@users.noreply.github.com>
2024-05-07 18:50:57 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-//QUESTION 1:
-console.log('Hello World');
-//QUESTION 2:Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-var personName = ('Habeeba');
-var message = ("how would you like to attend today's IT session, Online or Onsite?");
-console.log(personName + " " + message);
+//Q.3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+//uppercase, and title case.
+var personName = "Murk Bhatti";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-//QUESTION 1:
-console.log('Hello World');
+//Q.3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+//uppercase, and title case.
+let personName:string = "Murk Bhatti"
 
-//QUESTION 2:Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-//let personName = ('Habeeba')
-//let message = ("how would you like to attend today's IT session, Online or Onsite?")
-//console.log(personName+" "+message);
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+
+console.log(personName.replace(/\b\w/g,(char) => char.toUpperCase()));

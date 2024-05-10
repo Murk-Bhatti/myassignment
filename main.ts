3630fa84513248d1a734d647ae322215457ea557
Murk-Bhatti <165047752+Murk-Bhatti@users.noreply.github.com>
2024-05-10 19:11:51 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
-//Q.3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
-//uppercase, and title case.
-let personName:string = "Murk Bhatti"
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 
-console.log(personName.toLowerCase());
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 
-console.log(personName.toUpperCase());
+//console.log('Prophet Mohammad(P.B.U.H) once said:\"strive always to excel in virtue and truth.\"');
 
-console.log(personName.replace(/\b\w/g,(char) => char.toUpperCase()));
+//question 5
+//let famousPerson : string = "Prophet Mohammad(P.B.U.H)";
+//let message : string = "once said:\"strive always to excel in virtue and truth.\"";
+//console.log(famousPerson, message);
+
+//Question.6: Adding whitespace
+
+//let whitespace = '\n\t Murk Bhatti \n\t'
+//console.log(whitespace);
+//let stripwhitespaces = whitespace.trim();
+//console.log(stripwhitespaces);
+
+
+
